9e6812a69a8187b7d7e6807eefd6e20217f4fa55
pestevao <pestevao@sdilab.pt>
2024-04-11 09:25:50 +0100

    Sequencial version and output info

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define SRC_FILE "ggOR06.txt"
+#define SRC_FILE "ggOR03.txt"
 
 typedef struct task {
     int mach;
@@ -15,6 +15,25 @@ typedef struct job {
     task_t *task;
 } job_t;
 
+static void DUMPMATRIX(job_t ***jobs, int nJobs, int nTasks)
+{
+    for (int i = 0; i < nJobs; ++i) {
+        for (int j = 0; j < nTasks; ++j)
+            printf(" %2d %2d", (*jobs)[i]->task[j].mach, (*jobs)[i]->task[j].time);
+        printf("\n");
+    }
+}
+
+static void DUMPRESULT(int out[128][128], int nJobs, int nTasks)
+{
+    for (int i = 0; i < nJobs; ++i) {
+        for (int j = 0; j < nTasks; ++j)
+            printf(" %2d", out[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
 static void freeJobs(job_t **jobs, int nJobs) {
     if(!jobs)
         return;
@@ -25,9 +44,8 @@ static void freeJobs(job_t **jobs, int nJobs) {
     free(jobs);
 }
 
-bool loadMatrix(job_t ***jobs, int *nJobs) {
+static bool loadMatrix(job_t ***jobs, int *nJobs, int *nTasks) {
     const char *filename = SRC_FILE;
-    int cols = 0;
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -38,26 +56,24 @@ bool loadMatrix(job_t ***jobs, int *nJobs) {
         char *token = strtok(line, " ");
         *nJobs = atoi(token);
         token = strtok(NULL, " ");
-        cols = atoi(token);
+        *nTasks = atoi(token);
     }
-    const bool ok = (cols && nJobs);
+    const bool ok = (*nTasks && *nJobs);
     if (ok) {
         *jobs = (job_t **)calloc(*nJobs, sizeof(job_t *));
         for (int i = 0; i < *nJobs; ++i) {
             (*jobs)[i] = (job_t *)malloc(sizeof(job_t));
             (*jobs)[i]->jobId = i;
-            (*jobs)[i]->task = (task_t *)malloc(cols * sizeof(task_t));
-            printf("Job %d -", (*jobs)[i]->jobId);
+            (*jobs)[i]->task = (task_t *)malloc(*nTasks * sizeof(task_t));
+            //printf("Job %d -", (*jobs)[i]->jobId);
             if (fgets(line, sizeof(line), file) != NULL) {
                 const char *token = strtok(line, " ");
-                for (int j = 0; j < cols; ++j) {
+                for (int j = 0; j < *nTasks; ++j) {
                     (*jobs)[i]->task[j].mach = atoi(token);
                     token = strtok(NULL, " ");
                     (*jobs)[i]->task[j].time = atoi(token);
                     token = strtok(NULL, " ");
-                    printf(" %d %d", (*jobs)[i]->task[j].mach, (*jobs)[i]->task[j].time);
                 }
-                printf("\n");
             }
         }
     }
@@ -65,15 +81,40 @@ bool loadMatrix(job_t ***jobs, int *nJobs) {
     return ok;
 }
 
+static void runTask(int *startTime, int job, task_t *task)
+{
+    printf("Running Job %d on Machine %d\n", job, task->mach);
+    *startTime += task->time;
+}
+
+static void jobShopAlgorithm(job_t ***jobs, int out[128][128], int nJobs, int nTasks)
+{
+    int startTime = 0;
+    for (int i = 0; i < nJobs; ++i) {
+        for (int j = 0; j < nTasks; ++j) {
+            out[i][j] = startTime;
+            runTask(&startTime, i, &(*jobs)[i]->task[j]);
+        }
+        printf("\n");
+    }
+    printf("Duration Time %d\n\n", startTime);
+}
+
 int main() {
     job_t **jobs = NULL;
-    int nJobs = 0;
-    if (!loadMatrix(&jobs, &nJobs)) {
+    int nJobs = 0, nTasks = 0;
+    if (!loadMatrix(&jobs, &nJobs, &nTasks)) {
         printf("Failed to load matrix.\n");
         return 1;
     }
 
-    // TODO: JobShop algorythm here
+    DUMPMATRIX(&jobs, nJobs, nTasks);
+
+    /* JobShop algorythm */
+    int out[128][128];
+    jobShopAlgorithm(&jobs, out, nJobs, nTasks);
+
+    DUMPRESULT(out, nJobs, nTasks);
 
     freeJobs(jobs, nJobs);
     return 0;
